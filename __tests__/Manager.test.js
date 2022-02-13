7fb82ff481e2fe12612bb6bd477769d5682151a3
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-02-13 00:55:24 -0600

    added tests for the Manager class

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,32 @@
+const { expect } = require('@jest/globals');
+const { it } = require('jest-circus');
+const Manager = require('../lib/Manager');
+
+const manager = new Manager('cody', '5', 'griffinc6@gmail.com', '123');
+
+it('should get the costructor values from Manager', () => {
+    expect(manager.name).toBe('cody');
+    expect(manager.id).toBe('5');
+    expect(manager.email).toBe('griffinc6@gmail.com');
+    expect(manager.officeNumber).toBe('123');
+});
+
+it('should get the name from getName()', () => {
+    expect(manager.getName()).toBe('cody');
+});
+
+it('should get the id from getId()', () => {
+    expect(manager.getId()).toBe('5');
+});
+
+it('should get the email from getEmail()', () => {
+    expect(manager.getEmail).toBe('griffinc6@gmail.com');
+});
+
+it('should get the office number from getOfficeNumber()', () => {
+    expect(manager.getOfficeNumber).toBe('123');
+});
+
+it('should get the role from getRole()', () => {
+    expect(manager.getRole()).toBe('Manager');
+});
