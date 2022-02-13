111063c9d65a662226b9d5ae588ae3609a422e4e
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-02-13 01:33:05 -0600

    added tests and code for the Engineer class

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,30 @@
+const Engineer = require('../lib/Engineer');
+
+const engineer = new Engineer('cody', '5', 'griffinc6@gmail.com', 'cynogriffin');
+
+it('should get the constructor values for Engineer', () => {
+    expect(engineer.name).toBe('cody');
+    expect(engineer.id).toBe('5');
+    expect(engineer.email).toBe('griffinc6@gmail.com');
+    expect(engineer.github).toBe('cynogriffin');
+});
+
+it('should get the name from getName()', () => {
+    expect(engineer.getName()).toBe('cody');
+});
+
+it('should get the id from getId()', () => {
+    expect(engineer.getId()).toBe('5');
+});
+
+it('should get the email from getEmail()', () => {
+    expect(engineer.getEmail()).toBe('griffinc6@gmail.com');
+});
+
+it('should get the github username from getGithub()', () => {
+    expect(engineer.getSchool()).toBe('cynogriffin');
+});
+
+it('should get the role from getRole()', () => {
+    expect(engineer.getRole()).toBe('Engineer');
+});
