bdc44b138486af20e93a675ab69545f10d3f4f20
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-02-13 04:51:00 -0600

    fixed typos in html causing cards not to load correctly

--- src/generate-site.js
+++ src/generate-site.js
@@ -9,9 +9,9 @@ const createTeam = team => {
             ${manager.name} <br/>
             <i class="fas fa-mug-hot"></i> Manager</div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item>ID: ${manager.id}</li>
-                <li class="list-group-item>Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
-                <li class="list-group-item>Office Number: ${manager.officeNumber}</li>
+                <li class="list-group-item">ID: ${manager.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
+                <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
             </ul>
         </div>
         `;
@@ -26,9 +26,9 @@ const createTeam = team => {
             ${engineer.name} <br/>
             <i class="fas fa-glasses"></i> Engineer</div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item>ID: ${engineer.id}</li>
-                <li class="list-group-item>Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
-                <li class="list-group-item>GitHub Username: <a target="_blank" href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+                <li class="list-group-item">ID: ${engineer.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+                <li class="list-group-item">GitHub Username: <a target="_blank" href="https://github.com/${engineer.github}">${engineer.github}</a></li>
             </ul>
         </div>
         `;
@@ -43,9 +43,9 @@ const createTeam = team => {
             ${intern.name} <br/>
             <i class="fas fa-user-graduate"></i> Intern</div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item>ID: ${intern.id}</li>
-                <li class="list-group-item>Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
-                <li class="list-group-item>School: ${intern.school}</li>
+                <li class="list-group-item">ID: ${intern.id}</li>
+                <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+                <li class="list-group-item">School: ${intern.school}</li>
             </ul>
         </div>
         `;
